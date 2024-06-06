b3b519bd4ba81f64e15fc7b37c38e55e8d40a985
nishanth-B-shanker <72551114+nishanth-B-shanker@users.noreply.github.com>
2024-06-06 12:09:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const form = document.querySelector("form")
 const search = document.querySelector("input")
 const searchResult = document.querySelector(".results")
 
-const apiUrl = "https://en.wikipedia.org/w/api.php?action=query&list=search&prop=info&inprop=url&utf8=&format=json&origin=*&srlimit=20&srsearch="
+const apiUrl = "https://en.wikipedia.org/w/api.php?"
 
 
 form.addEventListener("submit", (e) => {
